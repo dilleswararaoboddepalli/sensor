e77db936c1869db875c8efef109cd4716746895b
dilleswararaoboddepalli <31070766+dilleswararaoboddepalli@users.noreply.github.com>
2017-08-28 17:07:20 +0530

    Create main2_activity.java

---
+++ main2_activity.java
@@ -0,0 +1,20 @@
+package com.example.dilleswararao.exam;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class Main2Activity extends AppCompatActivity {
+    public void goToActivity2 (View view){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+
+}
